25cebb995b68fe4cb93dd08e680454e2459f69cd
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-02-10 18:11:05 -0300

    Obtendo um usuário por ID

--- src/main/java/com/angelochiarella/workshopmongo/resources/UserResource.java
+++ src/main/java/com/angelochiarella/workshopmongo/resources/UserResource.java
@@ -1,13 +1,13 @@
 package com.angelochiarella.workshopmongo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.angelochiarella.workshopmongo.domain.User;
@@ -21,12 +21,19 @@ public class UserResource
 	@Autowired
 	private UserService service;
 
-//	@RequestMapping(method = RequestMethod.GET)
-	@GetMapping
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() // ResponseEntity - representa a resposta HTTP inteira.
 	{
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);// .ok() - instacia com codigo http sucesso | .body() - define corpo da resposta
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) //@PathVariable - Casa o id com id recebido da url
+	{
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
 }
---
+++ src/main/java/com/angelochiarella/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.angelochiarella.workshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.angelochiarella.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice // Responsável por tratar possiveis errors
+public class ResourceExceptionHandler
+{
+	@ExceptionHandler(ObjectNotFoundException.class) // Padrão para Identificar a Exception
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request)
+	{
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}

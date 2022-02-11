b5ce54cfa2f287ab5dcf653c1375e5f6294dea17
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-02-11 09:04:49 -0300

    Inserção de usuário com POST

--- src/main/java/com/angelochiarella/workshopmongo/resources/UserResource.java
+++ src/main/java/com/angelochiarella/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.angelochiarella.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.angelochiarella.workshopmongo.domain.User;
 import com.angelochiarella.workshopmongo.dto.UserDTO;
@@ -21,7 +24,7 @@ public class UserResource
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET) // @GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() // ResponseEntity - representa a resposta HTTP inteira.
 	{
 		List<User> list = service.findAll();
@@ -29,11 +32,22 @@ public class UserResource
 		return ResponseEntity.ok().body(listDto);// .ok() - instacia com codigo http sucesso | .body() - define corpo da resposta
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) //@PathVariable - Casa o id com id recebido da url
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // @GetMapping
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) // @PathVariable - Casa o id com id recebido da url
 	{
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@RequestMapping(method = RequestMethod.POST) // @PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) // @PathVariable - Casa o id com id recebido da url
+	{
+		User obj = service.fromDTO(objDto); // converter DTO para User
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+		// URI pega o endereço do novo objeto que for inserido
+		return ResponseEntity.created(uri).build();
+		// Retorna resposta vazia, com o código 201 com o cabeçalho contendo a localização do novo recurso criado
 	}
 
 }
--- src/main/java/com/angelochiarella/workshopmongo/services/UserService.java
+++ src/main/java/com/angelochiarella/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.angelochiarella.workshopmongo.domain.User;
+import com.angelochiarella.workshopmongo.dto.UserDTO;
 import com.angelochiarella.workshopmongo.repositories.UserRepository;
 import com.angelochiarella.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,15 @@ public class UserService
 	{
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj)
+	{
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) // instanciado na aqui para se precisar, poderá acessar o nd
+	{
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }

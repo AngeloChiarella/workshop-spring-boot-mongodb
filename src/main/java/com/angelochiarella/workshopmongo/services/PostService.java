76591e9cd5e43a134406153e863459f77bc59b02
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-02-11 14:04:46 -0300

    Obtendo um post por id

---
+++ src/main/java/com/angelochiarella/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.angelochiarella.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.angelochiarella.workshopmongo.domain.Post;
+import com.angelochiarella.workshopmongo.repositories.PostRepository;
+import com.angelochiarella.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService
+{
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id)
+	{
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}

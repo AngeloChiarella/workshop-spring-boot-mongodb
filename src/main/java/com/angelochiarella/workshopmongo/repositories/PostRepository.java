49fc0c5e9673f8ab070987f771472679e6b542d6
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-02-11 15:43:42 -0300

    Consulta simples com query methods

--- src/main/java/com/angelochiarella/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/angelochiarella/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.angelochiarella.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.angelochiarella.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>
 {
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	 // TitleContaining - Faz busca a partir do dado parâmetro, dentro de Title
+	 // IgnoreCase - Ignorar maiúsculas e minúsculas
 }
